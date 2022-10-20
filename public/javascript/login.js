e1e7e64cdd6de3c2a43a70d92e73550351f2ad5e
ConnieMarie <constance.marie.strahan@gmail.com>
2022-10-20 15:50:05 -0500

    login view/signup.js

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -9,9 +9,9 @@ async function loginFormHandler(event) {
       method: 'post',
       body: JSON.stringify({
         username,
-        password,
+        password
       }),
-      headers: { 'Content-Type': 'application/json' },
+      headers: { 'Content-Type': 'application/json' }
     });
 
     if (response.ok) {
@@ -22,36 +22,7 @@ async function loginFormHandler(event) {
   }
 }
 
-async function signupFormHandler(event) {
-  event.preventDefault();
 
-  const username = document.querySelector('#username-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
 
-  if (username && email && password) {
-    const response = await fetch('/api/users', {
-      method: 'post',
-      body: JSON.stringify({
-        username,
-        email,
-        password,
-      }),
-      headers: { 'Content-Type': 'application/json' },
-    });
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
 
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert(response.statusText);
-    }
-  }
-}
-
-document
-  .querySelector('.login-form')
-  .addEventListener('submit', loginFormHandler);
-
-document
-  .querySelector('.signup-form')
-  .addEventListener('submit', signupFormHandler);
