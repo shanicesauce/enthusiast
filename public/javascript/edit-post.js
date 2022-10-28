d92d95a1c094eded949fc5743abc63c7b039f909
ConnieMarie <constance.marie.strahan@gmail.com>
2022-10-28 16:26:01 -0500

    image and interest added to edit post

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -2,6 +2,8 @@ async function editFormHandler(event) {
   event.preventDefault();
 
   const post_text = document.querySelector('textarea[name=edit-post]').value.trim();
+  const interest = document.querySelector('#interest-signup').value;
+  const image = document.querySelector('input[name="image"]');
 
   const id = window.location.toString().split('/')[
     window.location.toString().split('/').length - 1
@@ -9,7 +11,9 @@ async function editFormHandler(event) {
   const response = await fetch(`/api/posts/${id}`, {
     method: 'PUT',
     body: JSON.stringify({
-      post_text
+      image: image.files[0],
+      post_text,
+      interest
     }),
     headers: {
       'Content-Type': 'application/json'
